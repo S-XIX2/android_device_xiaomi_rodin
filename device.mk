228b4ace6c57aaab51c281743f6b7833f118bb43
Omkar Parte <rakmoparte@gmail.com>
2026-03-08 17:28:40 +0530

    rodin: Update camera aux package list and add LMC8.3 GCam
    Update the camera auxiliary package list to include MeiyanCamera for
    both GCam and Aperture configurations. Additionally, integrate
    the LMC8.3 xenius9 mod (RC60) into the GCam build path.
    
    - device.mk: Add com.meitu.meiyancamera to vendor.camera.aux.packagelist.
    - GCam: Configure support for LMC8.3 xenius9 mod rc60.
    - Aperture: Sync auxiliary package list for consistency.
    
    Signed-off-by: Omkar Parte <88646966+RAM-UNLOK@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -29,10 +29,10 @@ ifeq ($(TARGET_SHIPS_MIUICAMERA), true)
 else ifeq ($(TARGET_SHIPS_GCAM), true)
     $(call inherit-product-if-exists, vendor/gcam/gcam.mk)
     PRODUCT_VENDOR_PROPERTIES += \
-        vendor.camera.aux.packagelist=com.ss.android.ugc.aweme,org.codeaurora.snapcam,com.agc.gcam88
+        vendor.camera.aux.packagelist=com.ss.android.ugc.aweme,org.codeaurora.snapcam,com.meitu.meiyancamera
 else
     PRODUCT_VENDOR_PROPERTIES += \
-        vendor.camera.aux.packagelist=org.lineageos.aperture
+        vendor.camera.aux.packagelist=org.lineageos.aperture,com.meitu.meiyancamera,com.ss.android.ugc.aweme
 endif
 
 # Inherit BCR
@@ -47,7 +47,7 @@ endif
 
 # Inherit ViPER4Android FX
 ifeq ($(TARGET_SHIPS_VIPERFX), true)
-$(call inherit-product-if-exists, packages/apps/ViPER4AndroidFX/config.mk)
+    $(call inherit-product-if-exists, packages/apps/ViPER4AndroidFX/config.mk)
 endif
 
 # Keys
